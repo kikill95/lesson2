e59acb8397be81293bf779684d9d822b80729df8
kikill95 <nokia6030>
2015-10-20 22:34:50 +0300

    Fix hash format

--- ball.rb
+++ ball.rb
@@ -6,26 +6,25 @@ class Ball
 
   def initialize
     @answers = YAML.load_file('answers.yml')
-    @colors = {:red => 31, :green => 32, :yellow => 33, :blue => 34}
+    @colors = { red: 31, green: 32, yellow: 33, blue: 34 }
   end
 
   def color(answer)
     case answer
-      when 0..4
-        @colors[:red]
-      when 5..9
-        @colors[:green]
-      when 10..14
-        @colors[:yellow]
-      else
-        @colors[:blue]
+    when 0..4
+      @colors[:red]
+    when 5..9
+      @colors[:green]
+    when 10..14
+      @colors[:yellow]
+    else
+      @colors[:blue]
     end
   end
 
   def shake
     random = rand(@answers.length)
     puts "\e[#{color(random)}m#{@answers[random]}\e[0m"
+    @answers[random]
   end
 end
-
-Ball.new.shake
