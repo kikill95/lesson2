1e240e467be491c56cbc1d7ddb041d1f3a0b369b
kikill95 <nokia6030>
2015-10-21 01:40:15 +0300

    Fix code for rubocop with bad decision

--- pet.rb
+++ pet.rb
@@ -1,7 +1,7 @@
 class Pet
-
   def initialize(name, type)
-    @name, @type = name, type
+    @name = name
+    @type = type
     @age = 0
     @mood = 10
     @hungry = 10
@@ -9,9 +9,8 @@ class Pet
     @clean = 10
     @walk = 10
     @energy = 10
-
     puts "You have created #{@type} with name: #{@name}."
-    puts "Type 'help' to watch full list of commands."
+    puts 'Type the word <help> to watch full list of commands.'
     menu
   end
 
@@ -85,11 +84,11 @@ class Pet
   def watch
     random = rand(10)
     if random == 3
-      puts "Your pet found a friend."
+      puts 'Your pet found a friend.'
       @mood += 4
     end
     if random == 5
-      puts "The car pined down your pet."
+      puts 'The car pined down your pet.'
       close
     end
     cycle
@@ -97,31 +96,31 @@ class Pet
   end
 
   def info
-    puts "Some information about your pet:"
-    puts "Your #{@type} has name: #{@name}"
-    puts "Age: #{@age}"
-    puts "Mood: #{@mood}"
-    puts "Hungry: #{@hungry}"
-    puts "Asleep: #{@asleep}"
-    puts "Clean: #{@clean}"
-    puts "Walk: #{@walk}"
-    puts "Energy: #{@energy}"
+    puts 'Some information about your pet:'
+    puts 'Your #{@type} has name: #{@name}'
+    puts 'Age: #{@age}'
+    puts 'Mood: #{@mood}'
+    puts 'Hungry: #{@hungry}'
+    puts 'Asleep: #{@asleep}'
+    puts 'Clean: #{@clean}'
+    puts 'Walk: #{@walk}'
+    puts 'Energy: #{@energy}'
     menu
   end
 
   def help
-    puts "The list of possible commands (type word or number):"
-    puts "1. walk"
-    puts "2. sleep"
-    puts "3. eat"
-    puts "4. play"
-    puts "5. clean"
-    puts "6. train"
-    puts "7. watch"
-    puts "8. punish"
-    puts "9. info"
-    puts "10. help"
-    puts "11. close"
+    puts 'The list of possible commands (type word or number):'
+    puts '1. walk'
+    puts '2. sleep'
+    puts '3. eat'
+    puts '4. play'
+    puts '5. clean'
+    puts '6. train'
+    puts '7. watch'
+    puts '8. punish'
+    puts '9. info'
+    puts '10. help'
+    puts '11. close'
     menu
   end
 
@@ -130,77 +129,79 @@ class Pet
   end
 
   def menu
-    puts "Your action:"
+    puts 'Your action:'
     action = gets.chomp
     case action
-    when "walk", "1"
+    when 'walk', '1'
       walk
-    when "sleep", "2"
+    when 'sleep', '2'
       sleep
-    when "eat", "3"
+    when 'eat', '3'
       eat
-    when "play", "4"
+    when 'play', '4'
       play
-    when "clean", "5"
+    when 'clean', '5'
       clean
-    when "train", "6"
+    when 'train', '6'
       train
-    when "watch", "7"
+    when 'watch', '7'
       watch
-    when "punish", "8"
+    when 'punish', '8'
       punish
-    when "info", "9"
+    when 'info', '9'
       info
-    when "help", "10"
+    when 'help', '10'
       help
-    when "close", "11"
+    when 'close', '11'
       close
-    else
     end
   end
-
 
   private
 
   def cycle
-    @age += 1
-    @mood -= rand(3)
-    @hungry -= rand(3)
-
     if @age > 20
-      puts "Your #{@type} #{@name} became very old and died."
+      puts "Your #{@type} #{@name} became a very old and died."
       close
     end
     if @mood > 25
-      puts "Your #{@type} #{@name} became crazy."
+      puts "Your #{@type} #{@name} became a crazy."
       close
     end
     if @hungry <= 0
-      puts "Your #{@type} #{@name} became very hungry and died."
+      puts "Your #{@type} #{@name} became a " \
+        'very hungry and died.'
       close
     end
     if @asleep <= 0
-      puts "Your #{@type} #{@name} became sick and died in the hospital."
+      puts "Your #{@type} #{@name} became a " \
+        'sick and died in the hospital.'
       close
     end
     if @clean <= 0
-      puts "Your #{@type} #{@name} became very unclean and beetles ate it."
+      puts "Your #{@type} #{@name} became a " \
+        'very unclean and beetles ate it.'
       close
     end
     if @walk <= 0
-      puts "Your #{@type} #{@name} became very sad and run away from you."
+      puts "Your #{@type} #{@name} became a " \
+        'very sad and run away from you.'
       close
     end
     if @energy <= 0
-      puts "Your #{@type} #{@name} had a nervous breakdown and died in the hospital."
+      puts "Your #{@type} #{@name} had a " \
+        'nervous breakdown and died in the hospital.'
       close
     end
+    @age += 1
+    @mood -= rand(3)
+    @hungry -= rand(3)
   end
 end
 
-puts "What type of animal do you want?"
+puts 'What type of animal do you want?'
 type = gets.chomp
-puts "What is the name of your pet?"
+puts 'What is the name of your pet?'
 name = gets.chomp
 
 Pet.new(type, name).menu
