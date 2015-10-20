32e980447545579b772572209e05e5f77ffb0fd0
kikill95 <nokia6030>
2015-10-20 22:14:23 +0300

    Replace require operator; add hash for colors; add RubyMonk's link

--- ball.rb
+++ ball.rb
@@ -1,27 +1,30 @@
+# RubyMonk's public profile: https://rubymonk.com/u/--15980
+require 'yaml'
+
 class Ball
-  require 'yaml'
-  attr_accessor :ANSWERS
+  attr_accessor :answers, :colors
 
   def initialize
-    @ANSWERS = YAML.load_file('answers.yml')
+    @answers = YAML.load_file('answers.yml')
+    @colors = {:red => 31, :green => 32, :yellow => 33, :blue => 34}
   end
 
   def color(answer)
     case answer
       when 0..4
-        31
+        @colors[:red]
       when 5..9
-        32
+        @colors[:green]
       when 10..14
-        33
+        @colors[:yellow]
       else
-        34
+        @colors[:blue]
     end
   end
 
   def shake
-    random = rand(@ANSWERS.length)
-    puts "\e[#{color(random)}m#{@ANSWERS[random]}\e[0m"
+    random = rand(@answers.length)
+    puts "\e[#{color(random)}m#{@answers[random]}\e[0m"
   end
 end
 
